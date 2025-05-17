d662b85f4a88e913df9d3e517eeb2e468179c857
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2025-05-17 12:48:57 -0300

    4. Median of Two Sorted Arrays

---
+++ 4_median_of_two_sorted_arrays.py
@@ -0,0 +1,8 @@
+from typing import List
+from statistics import median
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        return median(nums1)
